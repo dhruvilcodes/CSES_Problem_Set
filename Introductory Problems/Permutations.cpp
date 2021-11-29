b7dc9160b8f60a369d685ee25eee50d448289f3d
dhruvilcodes <dhruvilcodes@gmail.com>
2021-11-29 19:39:21 +0530

    First Commit

---
+++ Introductory Problems/Permutations.cpp
@@ -0,0 +1,94 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+#define MOD 1000000007
+
+
+ll mod_add(ll a, ll b, ll m = MOD) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;} 
+ll mod_mul(ll a, ll b, ll m = MOD) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+ll mod_sub(ll a, ll b, ll m = MOD) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+
+
+bool isPrime(ll n)
+{
+    if(n==1)
+    return false;
+    if(n==2 || n==3)
+    return true;
+    if(n%2==0 || n%3==0)
+    return false;
+    for(ll i=5; i*i<=n; i=i+6)
+    {
+    if(n % i == 0 || n % (i + 2) == 0)
+        return false; 
+    }
+        return true;
+}
+
+
+ll mfastPower(ll a, ll b, ll c=MOD)
+{
+ll res = 1;
+while (b > 0)
+{
+        if ((b & 1) != 0)
+        {
+          res = ((res % c) * (a % c) % c);
+        }
+         a = ((a % c) * (a % c)) % c;
+         b = b >> 1;
+    }
+    return res;
+    }
+
+
+void solve()
+{
+    ll n;
+    cin>>n;
+    if(n==1)
+
+    {
+        cout<<1;
+        return;
+    }
+    if(n<=3)
+    {
+            cout<<"NO SOLUTION";
+            return;
+    }
+        ll p=2;
+        while(p<=n)
+        {
+            cout<<p<<" ";
+            p=p+2;
+        }
+        //p=n-1;
+         p=1;
+        while(p<=n)
+        { 
+            cout<<p<<" ";
+            p=p+2;  
+        }
+
+
+}
+
+
+int main()
+{
+ ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+
+ ll t;
+ t=1;
+ while(t--)
+{
+ solve();
+cout<<endl;
+}
+
+
+
+return 0;
+}
